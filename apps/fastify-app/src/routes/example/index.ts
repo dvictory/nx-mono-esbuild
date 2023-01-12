d9996f665da5708ffdb91251faf7a6e2d6fd84b6
dvictory <derekvictory@gmail.com>
2023-01-12 14:52:54 -0600

    Add source map support

--- apps/fastify-app/src/routes/example/index.ts
+++ apps/fastify-app/src/routes/example/index.ts
@@ -3,9 +3,14 @@ import { isEven } from '@dbd/is-even';
 
 const example: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
   fastify.get('/', async function (request, reply) {
-    const now = new Date().getTime();
-    const even = isEven(now);
-    return `${now} is ${even ? '' : 'Not'} Even`;
+    try {
+      const now = new Date().getTime();
+      const even = isEven(now);
+      return `${now} is ${even ? '' : 'Not'} Even`;
+    } catch (e) {
+      console.log(e.stack);
+      throw e;
+    }
   });
 };
 
--- apps/fastify-app/src/routes/root.ts
+++ apps/fastify-app/src/routes/root.ts
@@ -4,6 +4,15 @@ const root: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
   fastify.get('/', async function (request, reply) {
     return { root: true };
   });
+
+  fastify.get('/error', async function (request, reply) {
+    try {
+      throw new Error('oops - see my map?')
+    } catch (e) {
+      return { stack: e.stack };
+    }
+
+  });
 };
 
 export default root;
