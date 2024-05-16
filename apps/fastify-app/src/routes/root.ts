52776b3d22078eed1c37a49b560449ff7de8bc60
dvictory <derekvictory@gmail.com>
2024-05-15 23:15:14 -0500

    NX 18 working

--- apps/fastify-app/src/routes/root.ts
+++ apps/fastify-app/src/routes/root.ts
@@ -1,17 +1,17 @@
 import { FastifyPluginAsync } from 'fastify';
+import { isEven } from '@dbd/is-even';
 
 const root: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
   fastify.get('/', async function (request, reply) {
-    return { root: true };
+    return { root: true, test: 'hello', even: isEven(2) };
   });
 
   fastify.get('/error', async function (request, reply) {
     try {
-      throw new Error('oops - see my map?')
+      throw new Error('oops - see my map?');
     } catch (e) {
       return { stack: e.stack };
     }
-
   });
 };
 
